4ea6db885cabda9ada877586dad5102fecf776ae
Akinuliola Akinbobola Oluwafemi <71355010+phemmyblaze@users.noreply.github.com>
2024-07-03 12:13:38 +0100

    Javascript class

---
+++ js/index.js
@@ -0,0 +1,61 @@
+// alert("Welcome to today's class");
+
+let firstName = "George"; ///CamelCase
+const KodeCamp = "kodecamp"; /// PascalCase
+const kode_camp = "kodecamp";
+let _kodecamp = "kodecamp";
+let FirstName;
+
+firstName = "Samson";
+
+const surname = "Emmanuel";
+
+var userName = "phemmyblaze";
+userName = "jamesBrown";
+
+console.log(userName);
+
+// alert("my name is " + firstName + " " + surname);
+
+firstName;
+
+let FIRSTNAME = "Akinuliola";
+
+const nationality = "Nigerian";
+let tribe = "calabar";
+tribe = "ondo";
+
+// nationality = "spanish";
+console.log(tribe);
+
+///Object
+let samson = {
+	firstName: "Samson",
+	surName: "John",
+	age: 30,
+	nationality: "spanish",
+	city: "Madrid",
+	drive: true,
+};
+console.log(samson.firstName);
+/// primitive
+
+let age = 30; ///Number
+let school = "OAU"; //// STRING
+let full = true; /// boolean
+let country; ///undefined
+let state = null; ///Null
+let PI = 3.1445;
+
+let BigInt = 3000000000;
+
+//basic operations addition, multiplication, division, subtract,
+
+const addition = 2024 * 1994;
+const multiplication = 2024 * 1994;
+const division = 2024 / 1994;
+const subtract = 2024 - 1994;
+console.log(addition, multiplication, division, subtract);
+
+console.log("This is sum of adding 2024 and 1994 together", +addition + " " + "on line 60"); /// concatenation
+console.log(`This is sum of adding 2024 and 1994 together ${addition} on line 61`); /// template literal
